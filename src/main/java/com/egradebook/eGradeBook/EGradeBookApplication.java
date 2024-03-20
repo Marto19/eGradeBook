0d90e99cc9ddd8d0043ce74f44d0570a879c8f89
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2024-03-20 14:22:23 +0200

    Initial commit

---
+++ src/main/java/com/egradebook/eGradeBook/EGradeBookApplication.java
@@ -0,0 +1,13 @@
+package com.egradebook.eGradeBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGradeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGradeBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egradebook/eGradeBook/EGradeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egradebook.eGradeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGradeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
