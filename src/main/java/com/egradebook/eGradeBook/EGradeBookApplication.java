b98f8a7b95b7fce99b51a0ab059e4f13884b75d0
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2024-04-16 13:49:23 +0300

    Update EGradeBookApplication.java
    Co-Authored-By: Hristo <35624178+MadForLife@users.noreply.github.com>

--- src/main/java/com/egradebook/eGradeBook/EGradeBookApplication.java
+++ src/main/java/com/egradebook/eGradeBook/EGradeBookApplication.java
@@ -11,4 +11,6 @@ public class EGradeBookApplication {
 		SpringApplication.run(EGradeBookApplication.class, args);
 	}
 
+	//todo: implement ABsences, Cirriculum, GradeCatalog
+	//todo: dual connection between teachers and junction table needs fixing
 }
