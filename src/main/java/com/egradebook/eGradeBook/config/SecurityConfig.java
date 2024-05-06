9f7957eb374d7c10064a0d6328dd8bc6cde2ed61
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2024-05-06 20:28:35 +0300

    Basic implementation of KeyCloak security - done
    -added a custom converter for the roles with prefix("ROLE_...")
    
    -cleaned securityConfig and modified it so that we use our own converter
    
    -added @PreAuthorize annotation to the demo controllers (This is the role that we have defined in the Keycloak server - the client roles)
    
    -added the resource-id and principle-attribute claims in the application properties, so that they're not embedded

--- src/main/java/com/egradebook/eGradeBook/config/SecurityConfig.java
+++ src/main/java/com/egradebook/eGradeBook/config/SecurityConfig.java
@@ -1,40 +1,25 @@
 package com.egradebook.eGradeBook.config;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity   //helps us add role based security to our application
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .anyRequest().authenticated()
-//                )
-//                .oauth2ResourceServer((oauth2) -> oauth2
-//                        .jwt(Customizer.withDefaults())
-//                );
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri) {
-//        return JwtDecoders.fromIssuerLocation(issuerUri);
-//    }
-
-
-
-
+    private final JwtAuthConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -43,7 +28,7 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated();
         http
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))); //this jwt has a default converter for ROLE_ prefix - JwtAuthenticationConverter and we need to override it with our own converter
         http
                 .sessionManagement((sessionManagement) ->
                         sessionManagement
@@ -52,45 +37,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-//
-//
-//
-//    //TODO: THE SECURITY CONFIGURATION BELOW IS TO BE FINISHED
-//
-//    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-//    String jwkSetUri;
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .anyRequest().authenticated()
-//                )
-//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-//                .addFilterAfter(createPolicyEnforcerFilter(), BearerTokenAuthenticationFilter.class);
-//        return http.build();
-//    }
-//
-//    private ServletPolicyEnforcerFilter createPolicyEnforcerFilter() {
-//        PolicyEnforcerConfig config;
-//
-//        try {
-//            config = JsonSerialization.readValue(getClass().getResourceAsStream("/policy-enforcer.json"), PolicyEnforcerConfig.class);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return new ServletPolicyEnforcerFilter(new ConfigurationResolver() {
-//            @Override
-//            public PolicyEnforcerConfig resolve(HttpRequest request) {
-//                return config;
-//            }
-//        });
-//    }
-//
-//    @Bean
-//    JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-//    }
-//
 }
