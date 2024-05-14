87a8ec3d080225d9c2ee7b712e042e2e549a9d21
Marto19 <bigguissss@gmail.com>
2024-05-14 17:48:57 +0300

    adding basic delete mapping

--- src/main/java/com/egradebook/eGradeBook/controllers/UserController.java
+++ src/main/java/com/egradebook/eGradeBook/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.egradebook.eGradeBook.controllers;
 
 import com.egradebook.eGradeBook.entities.User;
+import com.egradebook.eGradeBook.exceptions.ConflictException;
 import com.egradebook.eGradeBook.repositories.UserRepository;
 import com.egradebook.eGradeBook.services.UserService;
 import jakarta.validation.Valid;
@@ -59,6 +60,27 @@ public class UserController {
             userService.updateUser(user);
             return ResponseEntity.ok("User updated successfully");
         }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable long id)
+    {
+        //use find user by id to get the user
+        Optional<User> userOptional = userRepository.findUsersById(id);
+        if(userOptional.isPresent())
+        {
+            userService.deleteUser(id);
+            return ResponseEntity.ok("User deleted successfully");
+        }
+        //TODO: replace with the acutal methods that check if the user has any dependencies
+//        else if (!userOptional.getTeachers().isEmpty() || !userOptional.getPrincipals().isEmpty() || !user.getParents().isEmpty() || !user.getStudents().isEmpty()) {
+//            throw new ConflictException("Cannot delete user with id " + id + " because it has dependent entities");
+//        }
+
         else
         {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
---
+++ src/main/java/com/egradebook/eGradeBook/exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package com.egradebook.eGradeBook.exceptions;
+
+public class ConflictException extends Throwable {
+    public ConflictException(String s) {
+        super(s);
+    }
+}
