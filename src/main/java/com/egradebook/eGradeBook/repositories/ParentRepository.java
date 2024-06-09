718ebfc749fa609a8fa7e7612285364457f5fe25
Denis <f104378@students.nbu.bg>
2024-06-09 16:39:49 +0300

    parent update
    Show users that are not parents and not create user when adding parent

--- src/main/java/com/egradebook/eGradeBook/repositories/ParentRepository.java
+++ src/main/java/com/egradebook/eGradeBook/repositories/ParentRepository.java
@@ -12,7 +12,12 @@ import java.util.List;
 public interface ParentRepository extends JpaRepository<Parent, Long> {
 
     @Query("SELECT new com.egradebook.eGradeBook.DTOs.parent.ParentDTO(" +
-            "p.id, p.firstName, p.lastName, p.phoneNumber)" +
+            "p.id, p.user.firstName, p.user.lastName, p.user.phoneNumber)" +
             "FROM Parent p")
     List<ParentDTO> getParentsDTO();
+
+    @Query("SELECT p.user.id FROM Parent p")
+    List<Long> getParentUserIds();
+
+    boolean existsByUserId(Long userId);
 }
